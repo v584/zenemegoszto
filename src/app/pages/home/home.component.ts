a38494dd6a3e42e1210cb47c17f5a95ca2c1db10
v584 <sodium584@gmail.com>
2025-05-18 19:24:36 +0000

    deploysz

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MatButtonModule, MatCardModule, RouterLink],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
